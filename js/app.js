46a1b8eca48486e869819bb9f2ca7682dc3f97d2
Max Stern <stern.maxwell@gmail.com>
2016-08-13 16:49:22 -0500

    hamburger overlay.

--- js/app.js
+++ js/app.js
@@ -11,13 +11,15 @@ $('.explode').each(function() {
     var text = $(this).text();
     $(this).html(text.replace(/./g, "<span>$&</span>"));
 });
+
 $('.explode span').each(function() {
-var min = -200, max = 200,
-    min2 = -300, max2 = 300,
-    random = Math.floor(Math.random() * (max - min + 1)) + min,
+var min     = -200, max = 200,
+    min2    = -300, max2 = 300,
+    random  = Math.floor(Math.random() * (max - min + 1)) + min,
     random2 = Math.floor(Math.random() * (max2 - min2 + 1)) + min2,
-    css = "top:"+random+"px; left:"+random2+"px",
-    el = $(this);
+    css     = "top:"+random+"px; left:"+random2+"px",
+    el      = $(this);
+
     el.on({
         mouseenter: function () {
             el.attr("style", css);
@@ -28,4 +30,12 @@ var min = -200, max = 200,
             }, 300);
         }
     });
+});
+
+//hamburger
+var $hamburger = $(".hamburger");
+var $menu      = $(".menu");
+$hamburger.on("click", function(e) {
+  $hamburger.toggleClass("is-active");
+  $menu.toggleClass("fade");
 });
