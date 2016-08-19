a1bfdebb8dcf62c8401373d728e0aa5b8a93f0d2
Max Stern <stern.maxwell@gmail.com>
2016-08-18 22:04:23 -0500

    menu-active overflow injection, remove hamburger radius, work imagesLoaded .hide() .show(), lower menu font-size.

--- js/app.js
+++ js/app.js
@@ -8,34 +8,38 @@ $("hr").addClass("load");
 
 // Explosion
 $('.explode').each(function() {
-    var text = $(this).text();
-    $(this).html(text.replace(/./g, "<span>$&</span>"));
+  var text = $(this).text();
+  $(this).html(text.replace(/./g, "<span>$&</span>"));
 });
 
 $('.explode span').each(function() {
-var min     = -200, max = 200,
-    min2    = -300, max2 = 300,
-    random  = Math.floor(Math.random() * (max - min + 1)) + min,
+  var min = -200,
+    max = 200,
+    min2 = -300,
+    max2 = 300,
+    random = Math.floor(Math.random() * (max - min + 1)) + min,
     random2 = Math.floor(Math.random() * (max2 - min2 + 1)) + min2,
-    css     = "top:"+random+"px; left:"+random2+"px",
-    el      = $(this);
+    css = "top:" + random + "px; left:" + random2 + "px",
+    el = $(this);
 
-    el.on({
-        mouseenter: function () {
-            el.attr("style", css);
-        },
-        mouseleave: function () {
-            setTimeout(function() {
-            	el.removeAttr("style");
-            }, 300);
-        }
-    });
+  el.on({
+    mouseenter: function() {
+      el.attr("style", css);
+    },
+    mouseleave: function() {
+      setTimeout(function() {
+        el.removeAttr("style");
+      }, 300);
+    }
+  });
 });
 
 // Hamburger
 var $hamburger = $(".hamburger");
-var $menu      = $(".menu");
+var $menu = $(".menu");
+var $body = $("body");
 $hamburger.on("click", function(e) {
   $hamburger.toggleClass("is-active");
   $menu.toggleClass("fade");
+  $body.toggleClass("menu-active");
 });
