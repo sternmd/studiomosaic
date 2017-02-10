4a7f1a7b8fc07285be07905beb12cc8ac116fc74
Max Stern <stern.maxwell@gmail.com>
2017-02-09 18:46:46 -0600

    remove griblog

--- src/js/isotope.js
+++ src/js/isotope.js
@@ -42,38 +42,3 @@ $(".all").off('click.all').on('click.all', function(e) {
   e.preventDefault();
   $grid.isotope({ filter: '*' });
 });
-
-// //Isotope gridblog
-// var $gridblog = $('.gridblog').isotope({
-//   itemSelector: '.gridblog-item',
-//   sortBy: 'original-order',
-//   percentPosition: true,
-//   masonry: {
-//     // use outer width of grid-sizer for columnWidth
-//     columnWidth: '.gridblog-sizer'
-//   }
-// });
-//
-// $gridblog.imagesLoaded().progress( function() {
-//   $gridblog.fadeIn(1000).isotope('layout');
-// });
-//
-// $(".things").click(function() {
-//   // filter things items
-//   $gridblog.isotope({ filter: '.thing' });
-// });
-//
-// $(".places").click(function() {
-//   // filter places items
-//   $gridblog.isotope({ filter: '.place' });
-// });
-//
-// $(".peoples").click(function() {
-//   // filter people items
-//   $gridblog.isotope({ filter: '.people' });
-// });
-//
-// $(".all").click(function() {
-//   // show all items
-//   $gridblog.isotope({ filter: '*' });
-// });
