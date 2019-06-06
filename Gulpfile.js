9412c4b6d1195ad27713119788f120eb763bb547
Max Stern <stern.maxwell@gmail.com>
2019-06-06 11:06:19 -0700

    side trips vol 2 mix

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,75 +1,67 @@
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    clean       = require('gulp-clean'),
-    concat      = require('gulp-concat'),
-    jshint      = require('gulp-jshint'),
-    uglify      = require('gulp-uglify'),
-    notify      = require('gulp-notify'),
-    rename      = require('gulp-rename'),
-    plumber     = require('gulp-plumber'),
-    stylish     = require('jshint-stylish'),
-    browserSync = require('browser-sync'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    minify      = require('gulp-minify-css'),
-    open        = require('gulp-open');
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat'),
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  notify = require('gulp-notify'),
+  rename = require('gulp-rename'),
+  plumber = require('gulp-plumber'),
+  stylish = require('jshint-stylish'),
+  browserSync = require('browser-sync').create(),
+  sourcemaps = require('gulp-sourcemaps'),
+  minify = require('gulp-minify-css');
 
-gulp.task('clean-styles', function () {
-  return gulp.src('src/build/*.css', {read: false})
-    .pipe(clean());
+gulp.task('clean-styles', function() {
+  return gulp.src('src/build/*.css', { read: false }).pipe(clean());
 });
 
-gulp.task('clean-scripts', function () {
-  return gulp.src('src/build/*.js', {read: false})
-    .pipe(clean());
+gulp.task('clean-scripts', function() {
+  return gulp.src('src/build/*.js', { read: false }).pipe(clean());
 });
 
-gulp.task('styles', ['clean-styles'], function () {
-  return gulp.src('src/css/core.scss')
+gulp.task('styles', ['clean-styles'], function() {
+  return gulp
+    .src('src/css/core.scss')
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass({ style: 'expanded' }))
     .pipe(gulp.dest('src/css'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(minify())
     .pipe(sourcemaps.write('/maps'))
     .pipe(gulp.dest('src/build'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
 gulp.task('scripts', ['clean-scripts'], function() {
-  return gulp.src(['src/js/app.js'])
+  return gulp
+    .src(['src/js/app.js'])
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(jshint({
-      jquery: true
-    }))
+    .pipe(
+      jshint({
+        jquery: true
+      })
+    )
     .pipe(jshint.reporter(stylish))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
     .pipe(sourcemaps.write('/maps'))
     .pipe(gulp.dest('src/build'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browserSync.reload({ stream: true }))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 gulp.task('browser-sync', ['styles', 'scripts'], function() {
-    browserSync.init(null, {
-        startPath: "src/",
-        server: {
-            baseDir: './'
-        },
-        host: "localhost:8080"
-    });
-});
-
-gulp.task('open', function(){
-    var options = {
-        uri: 'localhost:8080',
-        app: 'google chrome'
-    };
-    gulp.src(__filename)
-        .pipe(open(options));
+  browserSync.init({
+    startPath: 'src/',
+    server: {
+      baseDir: './'
+    },
+    proxy: 'localhost:8080'
+  });
 });
 
 gulp.task('watch', function() {
@@ -78,9 +70,9 @@ gulp.task('watch', function() {
   // Watch .js files
   gulp.watch('src/js/*.js', ['scripts']);
   // Watch .html files
-  gulp.watch("src/**/*.html").on('change', browserSync.reload);
+  gulp.watch('src/**/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', function() {
-    gulp.start('styles', 'scripts', 'browser-sync', 'watch', 'open');
+  gulp.start('styles', 'scripts', 'browser-sync', 'watch');
 });
